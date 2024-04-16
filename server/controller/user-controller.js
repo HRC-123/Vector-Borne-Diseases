c9ff326400864a09443b43fcd4cd81df1ef4c73a
Chaitanya Reddy <chaitanyareddygujjula@gmail.com>
2024-04-16 12:24:45 +0530

    Resized analysis dashboard

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -63,7 +63,7 @@ exports.getTheData = async (req, res) => {
   const location = body.location;
   const from = body.from;
   const to = body.to;
-  let key = "EJ36RA47MPQKB6JB39SDUEKV8";
+  let key = "RPCF6N23JJSRWDKTBS8PUXZ6W";
 
   console.log(location, from, to);
   const response = await fetch(
